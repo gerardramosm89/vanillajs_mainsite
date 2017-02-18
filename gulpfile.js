1d3fa69eeb8ca11de434af05d49940d23f2db45a
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-02-18 15:14:36 -0800

    saving changes so far

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+
 gulp.task('default', function(){
   console.log("Hooray - you created a Gulp task");
 });
@@ -8,6 +13,19 @@ gulp.task('html', function(){
   console.log("Imagine something useful happening here");
 });
 
+gulp.task('styles', function(){
+  console.log("Imagine Sass or PostCSS tasks running here.");
+
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
 gulp.task('watch', function(){
-  watch('./app/index.html');
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  });
+  watch('./app/assets/styles/**.css', function(){
+    gulp.start('styles');
+  });
 });
--- vanillaServer.js
+++ vanillaServer.js
@@ -2,11 +2,13 @@ var express = require('express')
 var app = express()
 var path = require('path');
 
+app.use(express.static(__dirname + '/app'));
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/app/index.html'));
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8080!')
 })
 
